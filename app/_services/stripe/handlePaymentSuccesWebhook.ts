c297c688aa040bac6e54564a5b3dd075c3875780
Swiichy <adlpromail@gmail.com>
2023-08-29 22:25:06 +0200

    feat : implement stripe webhook

--- app/_services/stripe/handlePaymentSuccesWebhook.ts
+++ app/_services/stripe/handlePaymentSuccesWebhook.ts
@@ -1,23 +1,31 @@
+import { updateProductStock } from "@/sanity/lib/updateProductStock";
+
 export async function handlePaymentSuccessWebhook(event: any) {
   const paymentIntent = event.data.object;
 
-  console.log("Paiement réussi:", paymentIntent);
+  console.log("Paiement réussi:", paymentIntent.succeeded);
 
-  // Vérifier si le paiement est réussi
-  // if (paymentIntent.status === "succeeded") {
-  //   // Récupérer les informations nécessaires pour la mise à jour du stock
-  //   const productsToUpdate = getProductsFromPaymentIntent(paymentIntent);
+  if (paymentIntent.status === "succeeded") {
+    // Récupérer les informations nécessaires pour la mise à jour du stock
+    // const productsToUpdate = getProductsFromPaymentIntent(paymentIntent);
 
-  //   // Appeler la fonction pour mettre à jour le stock
-  //   const updateStockResult = await updateProductStock(productsToUpdate);
+    // Appeler la fonction pour mettre à jour le stock
+    const updateStockResult = await updateProductStock(
+      JSON.parse(paymentIntent.metadata)
+    );
 
-  //   // Gérer les erreurs ou enregistrer les informations
-  //   if (updateStockResult.error) {
-  //     console.error("Erreur lors de la mise à jour du stock:", updateStockResult.error);
-  //   } else {
-  //     console.log("Stock mis à jour avec succès.");
-  //   }
-  // } else {
-  //   console.log("Le paiement n'est pas en statut réussi, aucune action nécessaire.");
-  // }
+    // Gérer les erreurs ou enregistrer les informations
+    if (!updateStockResult) {
+      console.error(
+        "Erreur lors de la mise à jour du stock:",
+        paymentIntent.metadata
+      );
+    } else {
+      console.log("Stock mis à jour avec succès.");
+    }
+  } else {
+    console.log(
+      "Le paiement n'est pas en statut réussi, aucune action nécessaire."
+    );
+  }
 }
