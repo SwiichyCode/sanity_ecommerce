6ffd3223cfa9f7beddf163c2ee562164eede43f6
Swiichy <adlpromail@gmail.com>
2023-09-02 09:32:34 +0200

    fix: build error

--- app/(router)/(app)/profil/page.tsx
+++ app/(router)/(app)/profil/page.tsx
@@ -1,20 +1,9 @@
 "use client";
-import ProfileService from "@/app/_modules/auth/profile.service";
 
 export default function ProfilPage() {
-  const handleCreateOrder = async () => {
-    const { error } = await ProfileService.createOrder({
-      product: {
-        name: "test 2",
-      },
-    });
-    console.log(error);
-  };
-
   return (
     <div>
       <h1>Profil Page</h1>
-      <button onClick={handleCreateOrder}> add</button>
     </div>
   );
 }
