1914e090fafb76164b98a4b9f09177490e71ce9b
Swiichy <adlpromail@gmail.com>
2023-09-07 13:50:44 +0200

    feat: responsive menu cart

--- app/(router)/(app)/cart/_components/MenuCart/styles.ts
+++ app/(router)/(app)/cart/_components/MenuCart/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { breakpoints } from "@/app/_styles/breakpoints";
 
 export const MenuCartWrapper = styled.div`
   width: max-content;
@@ -8,7 +9,8 @@ export const MenuCartContainer = styled.div`
   position: absolute;
   top: 10.4rem;
   right: 2.4rem;
-  width: 40rem;
+  width: 100%;
+  max-width: 40rem;
   min-height: 40rem;
   display: flex;
   flex-direction: column;
@@ -18,4 +20,11 @@ export const MenuCartContainer = styled.div`
   padding: 2.4rem;
   box-shadow: var(--shadow-xl);
   border-radius: var(--rounded-lg);
+
+  @media (max-width: ${breakpoints.sm}) {
+    top: 8rem;
+    right: 0;
+    border-radius: 0;
+    max-width: 100%;
+  }
 `;
