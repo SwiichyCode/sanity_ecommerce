6cd1d63f7f60de6ce8aa24e3fac686957f99607d
Swiichy <adlpromail@gmail.com>
2023-09-08 18:10:38 +0200

    feat: create middleware for protect succes order page

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,22 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import stripe from "@/app/_lib/stripe/client";
 
-export function middleware(request: NextRequest) {}
+export async function middleware(request: NextRequest) {
+  const parsedUrl = request.nextUrl.searchParams.get("session_id");
+
+  const sessions = await stripe.checkout.sessions.list({
+    limit: 100,
+  });
+
+  const session = sessions.data.find((session) => session.id === parsedUrl);
+
+  if (session) {
+    console.log("valid session");
+  } else {
+    return NextResponse.redirect(new URL(`/cart`, request.nextUrl).href);
+  }
+}
+
+export const config = {
+  matcher: "/success",
+};
